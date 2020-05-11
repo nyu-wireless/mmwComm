d71b3058353e5833cd8af275a857b2fba7d6d9f2
Panagiotis Skrimponis <ps3857@nyu.edu>
2020-05-10 21:30:13 -0400

    add reference NR code and initial downlink demo

---
+++ +mmwsim/+antenna/InterpPatternAntenna.m
@@ -0,0 +1,54 @@
+classdef InterpPatternAntenna < matlab.System
+    % Wrapper class for an antenna to perform smooth interpolation of the
+    % directivity.  This class is necessary since the antenna toolbox
+    % generally performs nearest neighbor interpolation which may not
+    % be smooth
+    properties
+        
+        ant;  % Base antenna.  Must support a pattern method
+        fc;   % Frequency        
+        dirInterp;  % Gridded interplant
+    end
+    
+    methods
+        function obj = InterpPatternAntenna(ant, fc, varargin)
+            % Constructor
+            obj.ant = ant;
+            obj.fc = fc;
+            
+            % Set key-value pair arguments
+            if nargin >= 1
+                obj.set(varargin{:});
+            end
+        end
+    end
+        
+    methods (Access = protected)
+        
+        function setupImpl(obj)
+            % setup:  This is called before the first step.            
+            % We will use this point to create the interpolator.
+            
+            % TODO: Get the pattern from ant.pattern
+            [dirPat,azPat,elPat] = obj.ant.pattern(...
+                obj.fc,'Type', 'Directivity');                     
+            
+            % TODO:  Create the gridded interpolant object.  You can follow
+            % the demo in the antennas lecture
+            %     obj.dirInterp = griddedInterpolant(...)
+            obj.dirInterp = griddedInterpolant({elPat,azPat},dirPat);         
+            
+        end
+        function dir = stepImpl(obj, az, el)
+            % Computes the directivity along az and el angles in the local
+            % reference frame         
+            
+            % TODO:  Run the interplationn object to compute the directivity
+            % in the local angles
+            %   dir = obj.dirInterp(...)
+            dir = obj.dirInterp(el,az);
+            
+        end
+    end
+end
+
